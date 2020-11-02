e03dc8178cafdd9888ad18705e9a384aa57c3851
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-11-02 17:55:58 +0530

    Updates

---
+++ precache-manifest.6adde2d33c8efbc63daeb4c2196e13cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d53a5272f0f05cee137c8af0387b7aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b936c42bcd7c3b0056f",
+    "url": "/static/css/main.d9b8cb9b.chunk.css"
+  },
+  {
+    "revision": "6d2ad32d98f6994c06d7",
+    "url": "/static/js/2.84b30d85.chunk.js"
+  },
+  {
+    "revision": "a5eef939419e4dfc56a67e3b3cdb61c6",
+    "url": "/static/js/2.84b30d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b936c42bcd7c3b0056f",
+    "url": "/static/js/main.27aa185a.chunk.js"
+  },
+  {
+    "revision": "4f9b021f5e92e8ce7c49",
+    "url": "/static/js/runtime-main.8df6ef69.js"
+  }
+]);
